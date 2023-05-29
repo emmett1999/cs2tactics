4f6c963cec3975b25671979d17fc9a63beb60aa6
emmett1999 <83946630+emmett1999@users.noreply.github.com>
2023-05-29 00:10:45 -0500

    generate initial angular project
    * add basic angular material styling
    * create mat-toolbar header

---
+++ angular-cs2tactics/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
