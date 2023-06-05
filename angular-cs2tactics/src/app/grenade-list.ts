183a2379247cb6aa44cc2cc6502951d13e42dfe0
emmett1999 <83946630+emmett1999@users.noreply.github.com>
2023-06-04 23:08:46 -0500

    add dialog popup for grenades
    * also styling for the grenades popup
    * some css refactoring

--- angular-cs2tactics/src/app/grenade-list.ts
+++ angular-cs2tactics/src/app/grenade-list.ts
@@ -1,7 +1,7 @@
 import { Grenade } from "./grenade";
 
 export const GRENADES: Grenade[] = [
-    { title: "Smoke window from T spawn", type: "smoke", map: "mirage", contentUrl: "https://thumbs.gfycat.com/WhiteOptimisticGrackle-mobile.mp4"},
+    { title: "Smoke window from T spawn", type: "smoke", map: "mirage", contentUrl: "https://streamable.com/l/w4z08t/mp4.mp4"},
     { title: "Flash mid from top mid", type: "flash", map: "mirage", contentUrl: "https://thumbs.gfycat.com/WhiteOptimisticGrackle-mobile.mp4"},
     { title: "Smoke window from T spawn", type: "smoke", map: "nuke", contentUrl: "https://thumbs.gfycat.com/WhiteOptimisticGrackle-mobile.mp4"},
     { title: "Smoke window from T spawn", type: "smoke", map: "overpass", contentUrl: "https://thumbs.gfycat.com/WhiteOptimisticGrackle-mobile.mp4"},
--- angular-cs2tactics/src/app/nade-tile/nade-tile.component.ts
+++ angular-cs2tactics/src/app/nade-tile/nade-tile.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NadePopupComponent } from '../nade-popup/nade-popup.component';
+import { Grenade } from '../grenade';
 
 @Component({
   selector: 'nade-tile',
@@ -8,18 +9,22 @@ import { NadePopupComponent } from '../nade-popup/nade-popup.component';
   styleUrls: ['./nade-tile.component.css']
 })
 export class NadeTileComponent {
-    @Input() title: string;
-    @Input() type: string;
-    @Input() map: string;
-    @Input() contentUrl: string;
+    @Input() grenade: Grenade;
 
     constructor(public dialog: MatDialog){}
 
     openDialog() {
       this.dialog.open(NadePopupComponent, {
+        // TODO: Make the size of the dialog popup based on the screen size
+        height: '600px',
+        width: '854px',
         data: {
-          animal: 'panda',
+          title: this.grenade.title,
+          type: this.grenade.type,
+          map: this.grenade.map,
+          contentUrl: this.grenade.contentUrl,
         },
+        // panelClass: 'dialog-panel'
       });
     }
 }
